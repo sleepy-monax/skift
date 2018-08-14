92160644409a3134300001d89961b68bdc7c6152
Van Bossuyt Nicolas <vnicolas@localhost.localdomain>
2018-08-15 00:31:12 +0200

    Update Makefile

--- scripts/iso.mk
+++ scripts/iso.mk
@@ -1,6 +1,7 @@
-MKRESCUE = grub-mkrescue
+MKRESCUE = grub2-mkrescue
 
 image.iso: kernel.bin ramdisk.tar
-	cp kernel.bin iso/boot/kernel.bin
-	cp ramdisk.tar iso/boot/ramdisk.tar
-	$(MKRESCUE) -o image.iso iso
+	@echo -e "\033[1;37mBuilding the ISO...\033[0m"
+	@cp kernel.bin iso/boot/kernel.bin
+	@cp ramdisk.tar iso/boot/ramdisk.tar
+	@$(MKRESCUE) -o image.iso iso 2> /dev/null
--- scripts/ramdisk.mk
+++ scripts/ramdisk.mk
@@ -1,4 +1,4 @@
 
 ramdisk.tar:
-	@echo " .. Generating ramdisk"
-	tar -cvf ramdisk.tar -C base $(shell ls base/)
+	@echo -e "\033[1;37mGenerating the ramdisk...\033[0m"
+	@tar -cvf ramdisk.tar -C base $(shell ls base/)
