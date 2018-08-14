d7d8f07936efbe1157a6e18805245189cf951622
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2018-08-14 21:56:27 +0200

    working on makefile

--- scripts/iso.mk
+++ scripts/iso.mk
@@ -1,4 +1,6 @@
+MKRESCUE = grub-mkrescue
+
 image.iso: kernel.bin ramdisk.tar
 	cp kernel.bin iso/boot/kernel.bin
 	cp ramdisk.tar iso/boot/ramdisk.tar
-	grub2-mkrescue -o image.iso iso
+	$(MKRESCUE) -o image.iso iso
